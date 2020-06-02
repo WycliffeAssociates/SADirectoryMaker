e39028974cfee5769b39bcad55b9bd51e8cf367e
DJ <djpric91@gmail.com>
2020-06-02 15:58:17 -0400

    determining filename from full path and validating

--- src/main/kotlin/DirectoryLogic.kt
+++ src/main/kotlin/DirectoryLogic.kt
@@ -1,9 +1,11 @@
+import java.lang.IllegalArgumentException
+
 class DirectoryLogic {
 
     private val supportedExtensions = arrayOf("wav", "mp3", "jpeg", "tr")
 
     fun buildFullFilePath(
-        fileName: String,
+        inputFilePath: String,
         languageCode: String,
         dublinCoreId: String,
         group: String,
@@ -13,6 +15,18 @@ class DirectoryLogic {
     ): String {
 
         return ""
+
+    }
+
+    @Throws(IllegalArgumentException::class)
+    fun getFileNameFromFullPath(fullPath: String): String {
+
+        val extensionsString = supportedExtensions.joinToString(separator = "|", prefix = "(", postfix = ")")
+        val pattern = Regex("[a-z,0-9,_,~,\\.,\\-,/,\\\\]*.${extensionsString}", RegexOption.IGNORE_CASE)
+
+        if(fullPath.isBlank() || !fullPath.matches(pattern)) throw IllegalArgumentException("$fullPath is invalid")
+
+        return fullPath.split('/').last()
 
     }
 
