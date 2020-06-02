a7774cb3d7128726de7fc6935b09ef2aed8f6d0a
DJ <djpric91@gmail.com>
2020-06-02 15:29:51 -0400

    defining list of file extensions and creating function header for determining final file path

---
+++ src/main/kotlin/DirectoryLogic.kt
@@ -0,0 +1,19 @@
+class DirectoryLogic {
+
+    private val supportedExtensions = arrayOf("wav", "mp3", "jpeg", "tr")
+
+    fun buildFullFilePath(
+        fileName: String,
+        languageCode: String,
+        dublinCoreId: String,
+        group: String,
+        projectId: String = "",
+        mediaExtension: String = "",
+        mediaQuality: String = "hi"
+    ): String {
+
+        return ""
+
+    }
+
+}
