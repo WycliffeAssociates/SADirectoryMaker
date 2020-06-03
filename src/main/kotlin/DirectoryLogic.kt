faee949697ff614399f6eb2ccf3396fcc0574df2
Tony <34975907+AnonymousWalker@users.noreply.github.com>
2020-06-02 22:24:43 -0500

    refactor test file, add exception for empty "group" arg
    Split test file into two argument types: valid and invalid args.
    Add validation for empty group input.
    Add exception message for unsupported media extension.

--- src/main/kotlin/DirectoryLogic.kt
+++ src/main/kotlin/DirectoryLogic.kt
@@ -9,49 +9,51 @@ class DirectoryLogic {
 
     @Throws(IllegalArgumentException::class)
     fun buildFullFilePath(
-        inputFilePath: String,
-        languageCode: String,
-        dublinCoreId: String,
-        group: String,
-        projectId: String = "",
-        mediaExtension: String = "",
-        mediaQuality: String = ""
+            inputFilePath: String,
+            languageCode: String,
+            dublinCoreId: String,
+            group: String,
+            projectId: String = "",
+            mediaExtension: String = "",
+            mediaQuality: String = ""
     ): String {
 
         val fileExt = inputFilePath.split("/").last().split('.').last()
 
         var path = ""
 
-        if(languageCode.isBlank()) throw IllegalArgumentException("Language code is empty")
+        if (languageCode.isBlank()) throw IllegalArgumentException("Language code is empty")
         path += "$languageCode/"
 
-        if(dublinCoreId.isBlank()) throw IllegalArgumentException("Dublin Code ID is empty")
+        if (dublinCoreId.isBlank()) throw IllegalArgumentException("Dublin Code ID is empty")
         path += "$dublinCoreId/"
 
-        if(!projectId.isBlank()) path += "$projectId/"
-        path += "CONTENTS/"
+        if (group.isBlank()) throw IllegalArgumentException("Group is empty")
 
+        if (projectId.isNotBlank()) path += "$projectId/"
+
+        path += "CONTENTS/"
         path += "$fileExt/"
-        if(supportedContainers.contains(fileExt)) {
-            if(mediaExtension.isBlank()) throw IllegalArgumentException("Media Extension is empty")
-            if(!supportedExtensions.contains(mediaExtension)) throw IllegalArgumentException("Media Extension is not supported")
+
+        if (supportedContainers.contains(fileExt)) {
+            if (mediaExtension.isBlank()) throw IllegalArgumentException("Media Extension is empty")
+            if (!supportedExtensions.contains(mediaExtension)) throw IllegalArgumentException("Media Extension is not supported")
             path += "$mediaExtension/"
-        }
+        } else if (!supportedExtensions.contains(fileExt)) throw IllegalArgumentException(".$fileExt file is not supported")
 
         // if the file is a compressed type
-        if(compressedTypes.contains(fileExt) || compressedTypes.contains(mediaExtension)) {
-            if(mediaQuality.isBlank()) path += "hi/"
-            else if(mediaQuality == "hi" || mediaQuality == "low") path += "$mediaQuality/"
+        if (compressedTypes.contains(fileExt) || compressedTypes.contains(mediaExtension)) {
+            path += if (mediaQuality.isBlank()) "hi/"
+            else if (mediaQuality == "hi" || mediaQuality == "low") "$mediaQuality/"
             else throw IllegalArgumentException("Media Quality is invalid")
         }
 
-        if(!groupings.contains(group)) throw IllegalArgumentException("Group is not supported")
-        path += "$group/"
+        if (!groupings.contains(group)) throw IllegalArgumentException("Group is not supported")
 
+        path += "$group/"
         path += getFileNameFromFullPath(inputFilePath)
 
         return path
-
     }
 
     @Throws(IllegalArgumentException::class)
--- src/test/kotlin/DirectoryLogicTester.kt
+++ src/test/kotlin/DirectoryLogicTester.kt
@@ -12,26 +12,30 @@ class DirectoryLogicTester {
     @Test
     fun testBuildFullFilePath() {
 
-        val testFile: File = File("/home/dj/IdeaProjects/SADirectoryMaker/src/test/resources/TestCases.csv")
-        val testData = csvReader().readAll(testFile)
+        var testFile: File = File("/home/dj/IdeaProjects/SADirectoryMaker/src/test/resources/TestCases.csv")
+        var testData = csvReader().readAll(testFile)
 
-        for(i in 1 until testData.size) {
-            if(testData[i][7] == "IllegalArgumentException") {
-                assertThrows<IllegalArgumentException> {
-                    directoryLogic.buildFullFilePath(
-                        testData[i][0],
-                        testData[i][1],
-                        testData[i][2],
-                        testData[i][6],
-                        testData[i][3],
-                        testData[i][4],
-                        testData[i][5]
-                    )
-                }
-            } else {
-                assertEquals(
+        for (i in 1 until testData.size) {
+            assertEquals(
                     testData[i][7],
                     directoryLogic.buildFullFilePath(
+                            testData[i][0],
+                            testData[i][1],
+                            testData[i][2],
+                            testData[i][6],
+                            testData[i][3],
+                            testData[i][4],
+                            testData[i][5]
+                    ), "row $i"
+            )
+        }
+
+        testFile = File("/home/dj/IdeaProjects/SADirectoryMaker/src/test/resources/TestCasesException.csv")
+        testData = csvReader().readAll(testFile)
+
+        for (i in 1 until testData.size) {
+            val ex = assertThrows<IllegalArgumentException> {
+                directoryLogic.buildFullFilePath(
                         testData[i][0],
                         testData[i][1],
                         testData[i][2],
@@ -39,7 +43,6 @@ class DirectoryLogicTester {
                         testData[i][3],
                         testData[i][4],
                         testData[i][5]
-                    )
                 )
             }
         }
