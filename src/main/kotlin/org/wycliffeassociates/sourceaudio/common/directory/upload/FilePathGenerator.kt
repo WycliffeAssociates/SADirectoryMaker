178d432d471288ed187cde10c338a342cd7e199c
DJ <djpric91@gmail.com>
2020-06-04 15:29:27 -0400

    added file upload model data class in order to simplify file path generation function

--- src/main/kotlin/org/wycliffeassociates/sourceaudio/common/directory/upload/FilePathGenerator.kt
+++ src/main/kotlin/org/wycliffeassociates/sourceaudio/common/directory/upload/FilePathGenerator.kt
@@ -1,66 +1,36 @@
 package org.wycliffeassociates.sourceaudio.common.directory.upload
 
 import org.wycliffeassociates.sourceaudio.common.data.model.*
-import java.io.File
 import java.lang.IllegalArgumentException
 
 object FilePathGenerator {
 
     @Throws(IllegalArgumentException::class)
-    fun createPathFromFile(
-        inputFile: File,
-        languageCode: String,
-        dublinCoreId: String,
-        grouping: String,
-        projectId: String = "",
-        mediaExtension: String = "",
-        mediaQuality: String = "hi"
-    ): String {
-        validateInput(inputFile, languageCode, dublinCoreId, grouping, mediaExtension, mediaQuality)
+    fun createPathFromFile(fileModel: FileUploadModel): String {
+        val pathPrefix = getPathPrefix(
+            fileModel.languageCode,
+            fileModel.dublinCoreId,
+            fileModel.projectId,
+            fileModel.extension
+        )
 
-        val pathPrefix = getPathPrefix(languageCode, dublinCoreId, projectId, inputFile.extension)
-
-        val isContainer = ContainerExtensions.isSupported(inputFile.extension)
-        val isContainerAndCompressed = isContainer && CompressedExtensions.isSupported(mediaExtension)
-        val isFileAndCompressed = !isContainer && CompressedExtensions.isSupported(inputFile.extension)
+        val isContainer = ContainerExtensions.isSupported(fileModel.extension)
+        val isContainerAndCompressed = isContainer && CompressedExtensions.isSupported(fileModel.mediaExtension)
+        val isFileAndCompressed = !isContainer && CompressedExtensions.isSupported(fileModel.extension)
 
         return when {
-            isContainerAndCompressed -> "$pathPrefix/$mediaExtension/$mediaQuality/$grouping/${inputFile.name}"
-            isContainer -> "$pathPrefix/$mediaExtension/$grouping/${inputFile.name}"
-            isFileAndCompressed -> "$pathPrefix/$mediaQuality/$grouping/${inputFile.name}"
-            else -> "$pathPrefix/$grouping/${inputFile.name}"
-        }
-    }
-
-    @Throws(IllegalArgumentException::class)
-    private fun validateInput(
-        inputFile: File,
-        languageCode: String,
-        dublinCoreId: String,
-        grouping: String,
-        mediaExtension: String,
-        mediaQuality: String
-    ) {
-        if (languageCode.isBlank()) { throw IllegalArgumentException("Language Code is empty") }
-        if (dublinCoreId.isBlank()) { throw IllegalArgumentException("Dublin Core ID is empty") }
-        if (grouping.isBlank()) { throw IllegalArgumentException("Group is empty") }
-        if (!Groupings.isSupported(grouping)) { throw IllegalArgumentException("Group is not supported") }
-        if (!MediaQuality.isSupported(mediaQuality)) { throw IllegalArgumentException("Media Quality is invalid") }
-
-        validateExtensions(inputFile.extension, mediaExtension)
-    }
-
-    @Throws(IllegalArgumentException::class)
-    private fun validateExtensions(fileExtension: String, mediaExtension: String) {
-        if (ContainerExtensions.isSupported(fileExtension)) {
-            if (mediaExtension.isBlank()) {
-                throw IllegalArgumentException("Media Extension is empty")
+            isContainerAndCompressed -> {
+                "$pathPrefix/${fileModel.mediaExtension}/${fileModel.mediaQuality}/${fileModel.grouping}/${fileModel.name}"
             }
-            if (!CompressedExtensions.isSupported(mediaExtension) && !UncompressedExtensions.isSupported(mediaExtension)) {
-                throw IllegalArgumentException("Media Extension is not supported")
+            isContainer -> {
+                "$pathPrefix/${fileModel.mediaExtension}/${fileModel.grouping}/${fileModel.name}"
             }
-        } else if (!CompressedExtensions.isSupported(fileExtension) && !UncompressedExtensions.isSupported(fileExtension)) {
-            throw IllegalArgumentException(".${fileExtension} file is not supported")
+            isFileAndCompressed -> {
+                "$pathPrefix/${fileModel.mediaQuality}/${fileModel.grouping}/${fileModel.name}"
+            }
+            else -> {
+                "$pathPrefix/${fileModel.grouping}/${fileModel.name}"
+            }
         }
     }
 
